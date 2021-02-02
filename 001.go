1aab22f287388530f1ee4e1467d9c03dc68fa4bf
Shana12345 <shana_charlery@msn.com>
2021-02-02 03:48:54 +0000

    New file added

--- 001.go
+++ 001.go
@@ -3,23 +3,33 @@ package main
 import "fmt"
 import "encoding/json"
 import "log"
+import "io/ioutil"
 
 type Cat struct {
-	Name string
-	Age int
-	breed string
+	Name string `json:"adjustName"`
+  Animal string `json:"Breed"`
+	Age int `json:"age,omitempty"`
+  OwnerPayment string `json:"-"`
 }
 
 func main() {
-	cat := Cat{Name: "Charlie", Age: 3, breed: "Shakes"}
+	cat := Cat{Name: "Charlie", Animal: "Tabby", OwnerPayment: "it's totally a secret"}
 	cat.Speak()
-  b, err := json.Marshal(cat)
+  b, error := json.Marshal(cat)
 
-  log.Print(err)
-  log.Print(string(b)) 
+  log.Print(error)
+  log.Print(string(b))
+
+
+  RawJson, error2 := ioutil.ReadFile("MyJson.json")
+  log.Print(error2)
+
+  var C2 Cat
+  error3 := json.Unmarshal(RawJson, &C2)
+  log.Print(error3)
+  log.Printf("%+v", &C2)
 }
 
 func (c Cat) Speak() {
 	fmt.Println("MEOW")
-
 }
